9a9c4b828bc47363ba144d4b8fa3b3eee1f87a37
Paul Eisele <paulheisele@gmail.com>
2019-05-27 18:52:00 +0000

    Automatically backed up by Learn

---
+++ lib/players/computer.rb
@@ -0,0 +1,18 @@
+module Players
+  class Computer < Player
+
+    def move(board)
+      move = nil
+      if board.turn_count == 0
+        move = "5"
+      elsif board.turn_count == 1 && !board.taken?(5)
+        move = "5"
+      elsif board.turn_count == 1 && board.taken?(5)
+        move = [1, 3, 7, 9].detect {|i| !board.taken?(i)}.to_s
+      elsif board.turn_count >= 2
+        move = [*1..9].detect {|i| !board.taken?(i)}.to_s
+      end
+      move
+    end
+  end
+end
